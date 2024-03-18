ad85bba50a8c06d93227d5b653a0b0e34a54dbf1
Adam Porter <adamporter99@gmail.com>
2024-03-18 15:27:59 -0400

    add initial index back for deploy and destroy

--- s3.tf
+++ s3.tf
@@ -30,6 +30,12 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   )
 }
 
+resource "aws_s3_object" "file" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "index.html"
+  content_type = "text/html"
+}
+
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.bucket.id
 
